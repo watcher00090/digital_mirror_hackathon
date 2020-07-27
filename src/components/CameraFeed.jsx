594e813d6b220e16b202ebe7ef0403be6ba25f46
watcher00090 <james00090@gmail.com>
2020-07-27 15:18:53 -0700

    added files

--- src/components/CameraFeed.jsx
+++ src/components/CameraFeed.jsx
@@ -1,10 +1,23 @@
-import React, { useEffect, useRef } from 'react';
-import { Button, Col, Row } from 'react-bootstrap'
+import React, { useEffect, createRef } from 'react';
+import { Button, OverlayTrigger, Tooltip, Dropdown} from 'react-bootstrap'
+import { scryRenderedDOMComponentsWithTag } from 'react-dom/test-utils';
 
-const CameraFeed = () => {
-  const videoPlayer = useRef(null)
-  const canvas = useRef(null)
+class CameraFeed extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.videoPlayer = createRef()
+    this.canvas = createRef()
+
+    this.possibleEffects = ["none", "blur", "brightness", "contrast", 
+      "drop-shadow", "grayscale", "hue-rotate", "invert", "opacity", "saturate", "sephia", "url"];
+   
+    this.state="none";
+    this.setState({effect: "none"});
+  } 
+
+  /*
   useEffect(() => {
     console.log(navigator.mediaDevices.enumerateDevices())
     async function getCamera() {
@@ -14,73 +27,116 @@ const CameraFeed = () => {
     }
     getCamera()
   }, [])
+  */
 
-  const processDevices = (devices) => {
-    devices.forEach(device => {
-      console.log('Device:', device.label)
-      setDevice(device)
-    })
+
+ setDevice = async (device) => {
+  const { deviceId } = device
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { deviceId } })
+  .then(stream => this.videoPlayer.current.srcObject = stream)
+  .catch(err => console.log(err))
+  this.videoPlayer.current.play()
+}
+
+  processDevices = (devices) => {
+      devices.forEach(device => {
+        console.log('Device:', device.label)
+          this.setDevice(device)
+      })
   }
 
-  const setDevice = async (device) => {
-    const { deviceId } = device
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: { deviceId } })
-      .then(stream => videoPlayer.current.srcObject = stream)
-      .catch(err => console.log(err))
-    videoPlayer.current.play()
+  turnCameraOff = () => {
+    this.videoPlayer.current.srcObject.getVideoTracks().forEach(track => track.stop())
   }
 
-  const turnCameraOff = () => {
-    videoPlayer.current.srcObject.getVideoTracks().forEach(track => track.stop())
-  }
-
-  const turnCameraOn = async () => {
+  turnCameraOn = async () => {
     const cameras = await navigator.mediaDevices.enumerateDevices()
-    processDevices(cameras)
+    this.processDevices(cameras)
   }
 
-  const takePhoto = () => {
-    const context = canvas.current.getContext('2d')
-    context.drawImage(videoPlayer.current, 0, 0, canvas.current.width, canvas.current.height)
+  takePhoto = () => {
+      const context = this.canvas.current.getContext('2d')
+      var myEl = this.videoPlayer.current;
+      context.filter = myEl.style.filter;
+      
+      context.drawImage(myEl, 0, 0, this.canvas.current.width, this.canvas.current.height)
   }
 
+  changeEffect = (props) => {
+    if (false) {
+      console.log("Oops, effect not found");
+    } else {
+      this.setState({effect: props.effect});
+      console.log(this.state.effect);
+    }
+  };
 
-  const savePhoto = () => {
-    canvas.current.toBlob(
-      blob => {
-        const anchor = document.createElement('a')
-        anchor.download = 'my-file-name.jpg' // optional, but you can give the file a name
-        anchor.href = URL.createObjectURL(blob)
-        anchor.click(); // ✨ magic!
-        URL.revokeObjectURL(anchor.href) // remove it from memory and save on memory! 😎
-      },
-      'image/jpeg',
-      0.9,
+  getEffectText = function() {
+    console.log("getEffectText() called...")
+    switch(this.state.effect) {
+      case "none": 
+        return "";
+      case "blur":
+        return "blur(5px)";
+      case "brightness":
+        return "brightness(60%)"
+      case "contrast":
+        return "contrast(150%)"
+      case "drop-shadow":
+        return "drop-shadow(16px 16px 10px rgba(0,0,0,0.9))"
+      case "grayscale":
+        return "grayscale(200%)"
+      case "hue-rotate":
+        return "hue-rotate(90deg)"
+      case "invert":
+        return "invert(.8)"
+      case "opacity":
+        return "opacity(60%)";
+      case "saturate":
+        return "saturate(250%)";
+      case "sepia":
+        return "sepia(80%)"
+      default:
+        return "ERROR";
+    }
+  }
+
+  render() {
+      return (
+      <div className="c-camera-feed">
+        <div className="c-camera-feed__viewer">
+          <video id="videoPlayer" ref={this.videoPlayer} width="680" height="360" style={{filter: this.getEffectText()}}/>
+        </div>
+        <Button className='mr-2' onClick={() => this.turnCameraOff()}>Turn off Camera</Button>
+        <Button className='mr-2' onClick={() => this.turnCameraOn()}>Turn on Camera</Button>
+        <Button onClick={() => this.takePhoto()}>Take photo!</Button>
+
+        <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      Effects
+    </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "none"})}>none</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "blur"})}>blur</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "brightness"})}>brightness</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "contrast"})}>contrast</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "drop-shadow"})}>drop-shadow</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "grayscale"})}>grayscale</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "hue-rotate"})}>hue-rotate</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "invert"})}>invert</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "opacity"})}>opacity</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "saturate"})}>saturate</Dropdown.Item>
+        <Dropdown.Item onClick={() => this.changeEffect({effect: "sepia"})}>sephia</Dropdown.Item>
+      </Dropdown.Menu>
+     </Dropdown>
+
+        <div className="c-camera-feed__stage mt-2">
+          <canvas className='canvas' width="680" height="360" ref={this.canvas} />
+        </div>
+      </div>
     )
   }
 
-  return (
-    <div className="c-camera-feed">
-      <Row>
-        <Col>
-          <div className="c-camera-feed__viewer">
-            <video style={{ borderRadius: '3px' }} ref={videoPlayer} width="680" heigh="360" />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button className='btn-warning rounded mb-2 ' onClick={() => takePhoto()}>Take photo!</Button>
-        </Col>
-      </Row>
-      <Button className='btn-danger mr-2' onClick={() => turnCameraOff()}>Turn off Camera</Button>
-      <Button className='btn-success mr-2' onClick={() => turnCameraOn()}>Turn on Camera</Button>
-      <Button onClick={(e) => savePhoto(e)}>Save photo!</Button>
-      <div className="c-camera-feed__stage mt-2">
-        <canvas className='canvas' width="680" height="360" ref={canvas} />
-      </div>
-    </div>
-  )
 }
 
 export default CameraFeed
